2f8f52f53805f011a1a813e995eb481f713580a8
luca <lucafalasca08@gmail.com>
2024-02-06 19:04:43 +0100

    code smells

--- src/main/java/it/lucafalasca/Main.java
+++ src/main/java/it/lucafalasca/Main.java
@@ -1,27 +1,26 @@
 package it.lucafalasca;
 
+import it.lucafalasca.cold_start.ColdStart;
 import it.lucafalasca.enumerations.Project;
 import it.lucafalasca.populate.PopulateLocalFiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) throws IOException {
         Project project = Project.BOOKKEEPER;
-        //PopulateLocalFiles.populateLocalJsonClasses(project);
-
-
-        //PopulateLocalFiles.populateCSVFileV2(project);
-        //PopulateLocalFiles.tickets();
-        //ColdStart.coldStart();
-        //PopulateLocalFiles.populateCSVFile(project);
-        //PopulateLocalFiles.statsOnReleases(project);
-        //ArffMaker.csvToArff("src/main/resources/csv_files/DATASET_BOOKKEEPER_RELEASE_6_TEST_2024_02_05@10_53.csv");
-
+        PopulateLocalFiles.populateLocalJsonClasses(project);
+        PopulateLocalFiles.populateDatasets(project);
+        PopulateLocalFiles.statsOnReleases(project);
+        ColdStart.coldStart();
         try {
             PopulateLocalFiles.doExperiments(project);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error in experiments");
         }
     }
 }
